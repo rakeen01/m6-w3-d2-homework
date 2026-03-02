7d2709f3399d7b4b3f8d965f8c365f6de74d5de4
Md Rakeen Murtaza <rakeen.murtaza.12@gmail.com>
2026-03-02 15:59:36 -0800

    commit 2

--- router-params/src/App.js
+++ router-params/src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
+const streamingApps = [
+  {
+    slug: "netflix",
+    title: "Netflix",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/6/69/Netflix_logo.svg",
+    imageAlt: "Netflix"
+  },
+  {
+    slug: "hbo-max",
+    title: "HBO Max",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/1/17/HBO_Max_Logo.svg",
+    imageAlt: "HBO Max"
+  },
+  {
+    slug: "hulu",
+    title: "Hulu",
+    imageUrl: "https://i.pcmag.com/imagery/reviews/0142ww1h6aRqMkc4gP3zfUo-21..v1603983170.png",
+    imageAlt: "Hulu"
+  },
+  {
+    slug: "prime-video",
+    title: "Prime Video",
+    imageUrl: "https://upload.wikimedia.org/wikipedia/commons/f/f1/Prime_Video.png",
+    imageAlt: "Prime Video"
+  }
+];
+
+function formatSelection(appSlug) {
+  const selectedApp = streamingApps.find(({ slug }) => slug === appSlug);
+  return selectedApp ? selectedApp.title : appSlug.replace(/-/g, " ");
+}
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <section className="app-shell">
+        <h1>TV Apps</h1>
+        <div className="container">
+          <div className="row">
+            <ul>
+              {streamingApps.map(({ slug, title, imageUrl, imageAlt }) => (
+                <li key={slug}>
+                  <Link to={`/${slug}`} aria-label={title}>
+                    <img src={imageUrl} alt={imageAlt} />
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+        <Switch>
+          <Route path="/:appSlug" component={SelectedApp} />
+        </Switch>
+      </section>
+    </Router>
   );
 }
 
-export default App;
+function SelectedApp() {
+  const { appSlug } = useParams();
+  const activeAppName = formatSelection(appSlug);
+
+  return (
+    <section>
+      <h3>
+        You Selected:
+        <span>{activeAppName}</span>
+      </h3>
+    </section>
+  );
+}
